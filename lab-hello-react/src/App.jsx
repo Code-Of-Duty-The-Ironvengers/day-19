77e742a0823fb6723bb8b9ac6e9b674421681d79
Kira Soba <kira.soba@gmx.de>
2022-09-13 21:50:28 +0200

    Lab done

--- lab-hello-react/src/App.jsx
+++ lab-hello-react/src/App.jsx
@@ -1,34 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import Textbox from "./Textbox";
+
+const declarative = {
+  link: "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png",
+  headline: "Declarative",
+  text: "React makes it painless to create interactive UIs.",
+};
+const components = {
+  link: "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png",
+  headline: "Components",
+  text: "Build encapsulated components that manage their state.",
+};
+const singleWay = {
+  link: "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png",
+  headline: "Single-Way",
+  text: "A set of immutable values are passed to the component's.",
+};
+const jsxtext = {
+  link: "https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png",
+  headline: "JSX",
+  text: "Statically-typed, designed to run on modern browsers.",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="App"
+      style={{
+        width: "100%",
+        padding: "20px 40px 20px 20px",
+        backgroundColor: "#fff",
+      }}
+    >
+      <div style={{ backgroundColor: "#313C48", padding: "20px 50px" }}>
+        <nav
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginBottom: "100px",
+          }}
+        >
+          <img src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png"></img>
+          <img
+            src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/menu-top-xs.png"
+            style={{ height: "50%", alignSelf: "center" }}
+          ></img>
+        </nav>
+        <div style={{ width: "50%", textAlign: "left" }}>
+          <h1 style={{ margin: "0" }}>Say hello to ReactJS</h1>
+          <p>
+            You will learn how to use<br></br>the most popular frontend library,
+            <br></br>
+            and become a super Ninja developer.
+          </p>
+          <button
+            style={{
+              backgroundColor: "#fff",
+              color: "#313C48",
+              fontWeight: "bold",
+              borderRadius: "2px",
+              padding: "15px 20px",
+              margin: "20px 0 50px 0",
+            }}
+          >
+            Awesome!
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main
+        style={{
+          color: "#313C48",
+          display: "flex",
+          padding: "100px 50px",
+          gap: "50px",
+        }}
+      >
+        <Textbox {...declarative} />
+        <Textbox {...components} />
+        <Textbox {...singleWay} />
+        <Textbox {...jsxtext} />
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
