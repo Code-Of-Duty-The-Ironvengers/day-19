68a8553ce2e05fde8699bc7dadb22fbfdcf26db4
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-09-16 18:05:39 +0200

    completed the lab. add styling with bootstrap.

--- lab-hello-react/src/App.jsx
+++ lab-hello-react/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Feature from "./components/Feature";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <div style={{ background: "#16213E" }}>
+        <Navbar />
+        <Header />
+      </div>
+      <Feature />
+    </div>
+  );
 }
 
 export default App;
---
+++ lab-hello-react/src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <div class="header">
+      <h1 class="header-title">Say hello to ReactJS</h1>
+      <p class="header-text">
+        You will learn how to use the most popular frontend library, and become
+        a super Ninja developer.
+      </p>
+      <button class="header-btn">Awesome!</button>
+    </div>
+  );
+}
+
+export default Header;
