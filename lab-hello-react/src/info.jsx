be9975b5519f50cf91690709fc7d229fd7acdec4
Abhijeet Dalvi <abhijeetvdalvi@.com>
2022-09-22 21:29:30 +0200

    done

---
+++ lab-hello-react/src/info.jsx
@@ -0,0 +1,53 @@
+function Description() {
+  return (
+    <div className="cards">
+      <div className="box">
+        <img
+          width="75"
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png"
+        />
+        <h2>Declarative</h2>
+        <p>
+          React makes it <br></br>painless to create <br></br> interactive UIs.
+        </p>
+      </div>
+
+      <div className="box">
+        <img
+          width="75"
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png"
+        ></img>
+        <h2>Components</h2>
+        <p>
+          Build encapsulated <br></br>componenets that <br></br>manage their
+          state.
+        </p>
+      </div>
+
+      <div className="box">
+        <img
+          width="75"
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png"
+        ></img>
+        <h2>Single-Way</h2>
+        <p>
+          A set of immutable<br></br> values are passed to<br></br> the
+          component's{" "}
+        </p>
+      </div>
+
+      <div className="box">
+        <img
+          width="75"
+          src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png"
+        ></img>
+        <h2>JSX</h2>
+        <p>
+          Statically typed,<br></br> designed to run on <br></br>modern browsers
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Description;
